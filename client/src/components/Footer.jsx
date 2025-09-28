0306ee1834659ab12eaf456cd93bb2fd0ada124d
varunudata <varunudata@gmail.com>
2025-09-28 22:28:50 +0530

    bug fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
-        <img src={assets.logo} alt="" width={150}/>
+        <img src={assets.logo} alt="" width={60}/>
         <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @varunudata | All rights reserved.</p>
         <div className='flex gap-2.5 '>
             <img src={assets.instagram_icon} alt="" width={35}/>
