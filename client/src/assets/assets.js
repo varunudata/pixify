bce120920b16b496629e155a545d9ad986229615
varunudata <varunudata@gmail.com>
2025-09-29 23:53:36 +0530

    update

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -64,21 +64,21 @@ export const testimonialsData = [
     name: "Donald Jackman",
     role: "Graphic Designer",
     stars: 5,
-    text: `I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`,
+    text: `I've been using AI Text to Image generation app for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`,
   },
   {
     image: profile_img_2,
     name: "Richard Nelson",
     role: "Content Creator",
     stars: 4,
-    text: `I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`,
+    text: `I've been using AI Text to Image generation app for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`,
   },
   {
     image: profile_img_1,
     name: "Donald Jackman",
     role: " Graphic Designer",
     stars: 5,
-    text: `I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`,
+    text: `I've been using AI Text to Image generation app for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`,
   },
 ];
 
