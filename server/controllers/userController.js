fe3517d20cde11a0a1358205c8c33e12a194e352
varunudata <varunudata@gmail.com>
2026-04-26 13:00:09 +0530

    added CI/CD pipeline

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -135,7 +135,7 @@ const verifyRazorpay = async (req, res) => {
     const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
     if (orderInfo.status === "paid") {
       const transactionData = await transactionModel.findById(
-        orderInfo.receipt
+        orderInfo.receipt,
       );
       if (transactionData.payment) {
         return res.json({ success: false, message: "Payment Failed" });
